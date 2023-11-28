c91a17b7ea7cd01720ebb0779c5d9d181e2e6a53
vinr <galvinrasyad2007@gmail.com>
2023-11-28 13:26:13 +0700

    init project

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/signin', function () {
+    return view('signin');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/signin', function () {
+    return view('signin');
+});
